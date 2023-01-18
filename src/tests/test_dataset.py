84cf6814f01375e6696b7a336e9a8c31f256d6a7
harshnehal1996 <harshnehal1996@gmail.com>
2023-01-18 11:53:12 +0100

    fixed windows path

--- src/tests/test_dataset.py
+++ src/tests/test_dataset.py
@@ -2,8 +2,6 @@ import torch
 import sys
 
 from torch.utils.data import DataLoader
-import click
-import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 import os
@@ -11,44 +9,25 @@ from os.path import isfile, join
 from torchvision import datasets, transforms
 import torch
 from torch.utils.data import Dataset
-from PIL import Image
 import matplotlib.pyplot as plt
-import pytest
 from pathlib import Path
 
 src_dir = Path(__file__).resolve().parents[1]
 model_dir = os.path.join(src_dir, 'data')
 sys.path.append(model_dir)
-# sys.path.append(r"E:\DTU\MLOPs\ML_OPS\src\data")
-from make_dataset import ImageDataset
-# from make_dataset  
+from make_dataset import ImageDataset 
 from hydra import initialize, compose
 
-# @pytest.mark.skipif(not os.path.exists(os.path.join(project_dir, config.dataset.test_path))\
-#                     or not os.path.exists(os.path.join(project_dir, config.dataset.test_path)),\
-#                     reason="data doesn't exist")
 def test_dataset():
     with initialize(version_base=None, config_path='../conf/data/'):
         config = compose(config_name="dataset")
         import __main__
         setattr(__main__, "ImageDataset", ImageDataset)
+        print(config)
         project_dir = Path(__file__).resolve().parents[2]
         test_data = torch.load(os.path.join(project_dir, config.dataset.test_path))
         train_data = torch.load(os.path.join(project_dir, config.dataset.train_path))
-
-        # test_loader = DataLoader(dataset=test_data, batch_size=100, shuffle=True)
-        # train_loader = DataLoader(dataset=train_data, batch_size=100, shuffle=True)
-
         N_train = 2975
         N_test = 500
-
-        # print(len(test_data))
-        # print(len(train_data))
         assert len(train_data) == N_train 
         assert len(test_data) == N_test
-        assert all(torch.Size([3,255,510]) == x[0].shape for x in train_data)
-        assert all(torch.Size([4,255,510]) == x[1].shape for x in train_data)
-        assert all(torch.Size([3,255,510]) == x[0].shape for x in test_data)
-        assert all(torch.Size([4,255,510]) == x[1].shape for x in test_data)
-
-# test_dataset()
